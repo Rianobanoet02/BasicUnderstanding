20f39bcb1677bb4064a2c68c28d6ed7b62b2d082
Rianobanoet02 <rianobanoetb@gmail.com>
2024-12-20 21:54:07 +0800

    first commit

---
+++ penjumlahan.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 20 21:41:52 2024
+
+@author: Administrator
+"""
+
+a=int(input("masukan angka pertama "))
+b=int(input("masukan angka pertama "))
+print(f"Hasil penjumlahan:{a+b}")
+print(f"Hasil penjumlahan:{a-b}")
+print(f"Hasil penjumlahan:{a*b}")
+print(f"Hasil penjumlahan:{a/b}")
